2d577c3ed67bbdf501d5274a74cf0d65a0b3dd35
yayanyang <yayanyang@gmail.com>
2023-07-03 12:04:35 +0800

    update

--- src/ast.rs
+++ src/ast.rs
@@ -28,6 +28,21 @@ pub trait PhrasingContent {}
 pub enum Node {
     Document(Document),
     Heading(Heading),
+    ThematicBreak(ThematicBreak),
+    Blockquote(Blockquote),
+    List(List),
+    ListItem(ListItem),
+    Code(Code),
+    Definition(Definition),
+    Text(Text),
+    Emphasis(Emphasis),
+    Strong(Strong),
+    InlineCode(InlineCode),
+    Break(Break),
+    Link(Link),
+    LinkReference(LinkReference),
+    Image(Image),
+    ImageReference(ImageReference),
 }
 
 impl Debug for Node {
@@ -35,6 +50,21 @@ impl Debug for Node {
         match self {
             Node::Document(x) => x.fmt(f),
             Node::Heading(x) => x.fmt(f),
+            Node::ThematicBreak(x) => x.fmt(f),
+            Node::Blockquote(x) => x.fmt(f),
+            Node::List(x) => x.fmt(f),
+            Node::ListItem(x) => x.fmt(f),
+            Node::Code(x) => x.fmt(f),
+            Node::Definition(x) => x.fmt(f),
+            Node::Text(x) => x.fmt(f),
+            Node::Emphasis(x) => x.fmt(f),
+            Node::Strong(x) => x.fmt(f),
+            Node::InlineCode(x) => x.fmt(f),
+            Node::Break(x) => x.fmt(f),
+            Node::Link(x) => x.fmt(f),
+            Node::LinkReference(x) => x.fmt(f),
+            Node::Image(x) => x.fmt(f),
+            Node::ImageReference(x) => x.fmt(f),
         }
     }
 }
@@ -45,6 +75,21 @@ impl Node {
         match self {
             Node::Document(x) => visitor.visit_document(x),
             Node::Heading(x) => visitor.visit_heading(x),
+            Node::ThematicBreak(x) => visitor.visit_thematic_break(x),
+            Node::Blockquote(x) => visitor.visit_blockquote(x),
+            Node::List(x) => visitor.visit_list(x),
+            Node::ListItem(x) => visitor.visit_list_item(x),
+            Node::Code(x) => visitor.visit_code(x),
+            Node::Definition(x) => visitor.visit_definition(x),
+            Node::Text(x) => visitor.visit_text(x),
+            Node::Emphasis(x) => visitor.visit_emphasis(x),
+            Node::Strong(x) => visitor.visit_strong(x),
+            Node::InlineCode(x) => visitor.visit_inline_code(x),
+            Node::Break(x) => visitor.visit_break(x),
+            Node::Link(x) => visitor.visit_link(x),
+            Node::LinkReference(x) => visitor.visit_link_reference(x),
+            Node::Image(x) => visitor.visit_image(x),
+            Node::ImageReference(x) => visitor.visit_image_reference(x),
         }
     }
 }
@@ -55,6 +100,36 @@ pub trait Visitor {
     fn visit_document(&mut self, document: &Document) {}
 
     fn visit_heading(&mut self, heading: &Heading) {}
+
+    fn visit_thematic_break(&mut self, thematic_break: &ThematicBreak) {}
+
+    fn visit_blockquote(&mut self, blockquote: &Blockquote) {}
+
+    fn visit_list(&mut self, node: &List) {}
+
+    fn visit_list_item(&mut self, node: &ListItem) {}
+
+    fn visit_code(&mut self, node: &Code) {}
+
+    fn visit_definition(&mut self, node: &Definition) {}
+
+    fn visit_text(&mut self, node: &Text) {}
+
+    fn visit_emphasis(&mut self, node: &Emphasis) {}
+
+    fn visit_strong(&mut self, node: &Strong) {}
+
+    fn visit_inline_code(&mut self, node: &InlineCode) {}
+
+    fn visit_break(&mut self, node: &Break) {}
+
+    fn visit_link(&mut self, node: &Link) {}
+
+    fn visit_link_reference(&mut self, node: &LinkReference) {}
+
+    fn visit_image(&mut self, node: &Image) {}
+
+    fn visit_image_reference(&mut self, node: &ImageReference) {}
 }
 
 /// Parent (UnistParent) represents an abstract interface in
@@ -262,3 +337,230 @@ pub struct ListItem {
 parent!(ListItem, FlowContent);
 
 impl ListContent for ListItem {}
+
+/// Code (Literal) represents a block of preformatted text, such as ASCII art or computer code.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "code")
+)]
+pub struct Code {
+    /// Literal data.
+    pub value: String,
+    /// [`Option`] field to indicate code language.
+    pub lang: Option<String>,
+    /// Meta data for code language.
+    pub meta: Option<String>,
+}
+
+impl FlowContent for Code {}
+
+/// Code (Literal) represents a block of preformatted text, such as ASCII art or computer code.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "definition")
+)]
+pub struct Definition {
+    /// Children node list.
+    pub children: Vec<Node>,
+    /// An identifier field must be present. It can match another node.
+    /// identifier is a source value: character escapes and character
+    /// references are not parsed. Its value must be normalized.
+    pub identifier: String,
+    /// A label field can be present.
+    /// label is a string value: it works just like title on a link or a lang on
+    /// code: character escapes and character references are parsed.
+    pub label: Option<String>,
+    /// A url field must be present. It represents a URL to the referenced resource.
+    pub url: String,
+    /// A title field can be present.
+    /// It represents advisory information for the resource,
+    /// such as would be appropriate for a tooltip.
+    pub title: Option<String>,
+}
+
+impl FlowContent for Definition {}
+
+/// Text (Literal) represents everything that is just text.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "text")
+)]
+pub struct Text {
+    /// Text literal value
+    pub value: String,
+}
+/// Text can be used where phrasing content is expected.
+/// Its content is represented by its value field.
+impl PhrasingContent for Text {}
+
+/// Emphasis (Parent) represents stress emphasis of its contents.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "emphasis")
+)]
+pub struct Emphasis {
+    /// Children node list.
+    pub children: Vec<Node>,
+}
+
+parent!(Emphasis, PhrasingContent);
+
+/// Emphasis can be used where phrasing content is expected.
+/// Its content model is phrasing content.
+impl PhrasingContent for Emphasis {}
+
+/// Strong (Parent) represents strong importance, seriousness, or urgency for its contents.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "strong")
+)]
+pub struct Strong {
+    /// Children node list.
+    pub children: Vec<Node>,
+}
+
+parent!(Strong, PhrasingContent);
+
+/// Strong can be used where phrasing content is expected.
+/// Its content model is phrasing content.
+impl PhrasingContent for Strong {}
+
+/// InlineCode (Literal) represents a fragment of computer code, such as a file name,
+/// computer program, or anything a computer could parse.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "inlinecode")
+)]
+pub struct InlineCode {
+    /// Text literal value
+    pub value: String,
+}
+/// InlineCode can be used where phrasing content is expected.
+/// Its content is represented by its value field.
+impl PhrasingContent for InlineCode {}
+
+/// InlineCode (Literal) represents a fragment of computer code, such as a file name,
+/// computer program, or anything a computer could parse.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "break")
+)]
+pub struct Break {}
+/// Break can be used where phrasing content is expected.
+/// Its content is represented by its value field.
+impl PhrasingContent for Break {}
+
+/// Link (Parent) represents a hyperlink.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "link")
+)]
+pub struct Link {
+    /// Children node list.
+    pub children: Vec<Node>,
+    /// A url field must be present. It represents a URL to the referenced resource.
+    pub url: String,
+    /// A title field can be present.
+    /// It represents advisory information for the resource,
+    /// such as would be appropriate for a tooltip.
+    pub title: Option<String>,
+}
+
+parent!(Link, PhrasingContent);
+
+/// Link can be used where phrasing content is expected.
+/// Its content model is phrasing content.
+impl PhrasingContent for Link {}
+
+/// Link (Parent) represents a hyperlink.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "link")
+)]
+pub struct LinkReference {
+    /// Children node list.
+    pub children: Vec<Node>,
+    /// An identifier field must be present. It can match another node.
+    /// identifier is a source value: character escapes and character
+    /// references are not parsed. Its value must be normalized.
+    pub identifier: String,
+    /// A label field can be present.
+    /// label is a string value: it works just like title on a link or a lang on
+    /// code: character escapes and character references are parsed.
+    pub label: Option<String>,
+    /// A referenceType field must be present. Its value must be a referenceType.
+    /// It represents the explicitness of the reference.
+    pub reference_type: String,
+}
+
+parent!(LinkReference, PhrasingContent);
+
+/// LinkReference can be used where phrasing content is expected.
+/// Its content model is phrasing content.
+impl PhrasingContent for LinkReference {}
+
+/// Image (Node) represents an image.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "image")
+)]
+pub struct Image {
+    /// A url field must be present. It represents a URL to the referenced resource.
+    pub url: String,
+    /// A title field can be present.
+    /// It represents advisory information for the resource,
+    /// such as would be appropriate for a tooltip.
+    pub title: Option<String>,
+    /// An alt field should be present.
+    /// It represents equivalent content for environments
+    /// that cannot represent the node as intended.
+    pub alt: Option<String>,
+}
+
+/// Image can be used where phrasing content is expected.
+/// Its content model is phrasing content.
+impl PhrasingContent for Image {}
+
+/// Image (Node) represents an image.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(tag = "type", rename = "strong")
+)]
+pub struct ImageReference {
+    /// A url field must be present. It represents a URL to the referenced resource.
+    pub url: String,
+    /// A title field can be present.
+    /// It represents advisory information for the resource,
+    /// such as would be appropriate for a tooltip.
+    pub title: Option<String>,
+    /// An alt field should be present.
+    /// It represents equivalent content for environments
+    /// that cannot represent the node as intended.
+    pub alt: Option<String>,
+}
+
+/// ImageReference can be used where phrasing content is expected.
+/// Its content model is phrasing content.
+impl PhrasingContent for ImageReference {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
 /// [mdast](https://github.com/syntax-tree/mdast#list) implementation
 pub mod ast;
+
+/// markdown lexical grammar parser.
+pub mod lexer;
